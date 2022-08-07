2f6fdf3c031e3f56df40962c4932fdcd97e0701b
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-08-07 17:48:44 +0530

    update: Added User data

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,8 @@
 import { createContext, useContext } from "react";
 import { GoogleAuthProvider,signInWithPopup,signOut,onAuthStateChanged } from "firebase/auth";
 import {auth} from "../firebase"
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 const AuthContext = createContext();
 
@@ -9,9 +11,29 @@ const AuthProvider = ({children})=>{
         const provider = new GoogleAuthProvider();
         signInWithPopup(auth,provider)
     }
-    
+
+    const [userData,setUserData] = useState({});
+    const [user,setUser] = useState({});
+
+    const logOut =()=>{
+        signOut(auth)
+    }
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth,(currentUser)=>{
+            setUser(currentUser)
+        });
+        return ()=>{
+            unsubscribe();
+        }
+        }
+    ,[])
+
+    useEffect(()=>{
+        const url = "https://mocki.io/v1/bb11aecd-ba61-44b9-9e2c-beabc442d818";
+        axios(url).then(res=>setUserData(res.data)).catch(err=>console.log(err))
+    },[])
     return(
-        <AuthContext.Provider value={{googleSignIn}}>
+        <AuthContext.Provider value={{googleSignIn,userData,user,logOut}}>
             {children}
         </AuthContext.Provider>
     )
