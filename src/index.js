bedc320ac5fb6d023f95d7243021c6f02bc5ae15
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-08-07 17:47:58 +0530

    added: Context and Routes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
