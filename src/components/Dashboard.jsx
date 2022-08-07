44a1024921e101f3e58f5aa3e4f8544d9d7bc637
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-08-07 17:55:03 +0530

    fix: css of dashboard Overview section

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,7 +8,7 @@ export const Dashboard =()=>{
                 <div className="border outline-2 m-2 p-2">
                     <p className="text-xl font-medium py-2">Overview</p>
                     <div className="grid grid-cols-2 grid-rows-2 gap-4 justify-center">
-                        <div className="p-4 bg-[#F6F7FF] border outline-2 rounded-lg font-semibold flex justify-center items-center">Profile Views <span className="text-[#4F65F6] text-3xl font-medium ml-2">{userData.dashboard_stats.profile_views}</span></div>
+                        <div className="p-4 bg-[#F6F7FF] border outline-2 rounded-lg font-semibold flex items-center">Profile Views <span className="text-[#4F65F6] text-3xl font-medium ml-2">{userData.dashboard_stats.profile_views}</span></div>
                         <div className="p-4 bg-[#F6F7FF] border outline-2 rounded-lg font-semibold flex">Mentorship Session <span className="text-[#4F65F6] text-3xl font-medium ml-2">{userData.dashboard_stats.mentorship_sessions}</span></div>
                         <div className="p-4 bg-[#F6F7FF] border outline-2 rounded-lg font-semibold flex">Jobs Applied <span className="text-[#4F65F6] text-3xl font-medium ml-4">{userData.dashboard_stats.jobs_applied}</span></div>
                         <div className="p-4 bg-[#F6F7FF] border outline-2 rounded-lg font-semibold flex">Skills Verified <span className="text-[#4F65F6] text-3xl font-medium ml-4">{userData.dashboard_stats.skills_verified}</span></div>
