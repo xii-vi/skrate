f37ce1e5e86c85feeb69a70ec37ef25bf30b7693
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-08-07 21:11:46 +0530

    indentation: used prettier

--- src/components/NavbarSignIn.jsx
+++ src/components/NavbarSignIn.jsx
@@ -1,12 +1,12 @@
-import logo from "../assest/logo.png"
-import navbarImg from "../assest/navbar.png"
-export const NavbarSignIn = ()=>{
-    return(
-        <nav className="flex h-80">
-        <img className="w-20 h-20 lg:w-24 lg:h-24" src={logo} alt="logo" />
-        <div className="mx-auto">
-            <img className ="lg:w-screen lg:h-80" src={navbarImg} alt="nav-img" />
-        </div>
+import logo from "../assest/logo.png";
+import navbarImg from "../assest/navbar.png";
+export const NavbarSignIn = () => {
+  return (
+    <nav className="flex h-80">
+      <img className="w-20 h-20 lg:w-24 lg:h-24" src={logo} alt="logo" />
+      <div className="mx-auto">
+        <img className="lg:w-screen lg:h-80" src={navbarImg} alt="nav-img" />
+      </div>
     </nav>
-    )
-}
+  );
+};
