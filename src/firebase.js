0d78d7ee79a4393be4cab9642e780a11d0dc97f3
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-08-07 14:58:44 +0530

    authentication: Added firebase for google login

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDb46dQ_aGRAYEyJ40CENzVRdjV0Ct8NWQ",
+  authDomain: "skrrrrt-9053c.firebaseapp.com",
+  projectId: "skrrrrt-9053c",
+  storageBucket: "skrrrrt-9053c.appspot.com",
+  messagingSenderId: "762738747835",
+  appId: "1:762738747835:web:243c056f2a851c3be93c9e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
