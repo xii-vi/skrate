cb551a7885b73c84ca2cc827969fa7574e8555d3
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-08-07 17:48:22 +0530

    Added: Public And Private Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { SignInPage } from "./pages/SignInPage";
+import {Homepage} from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+
 function App() {
   return (
-    <></>
+    <>
+    <Routes>
+    <Route path="/" element={ <SignInPage />} />
+    <Route path="/dashboard" element={<ProtectedRoutes><Homepage /></ProtectedRoutes> } />
+    </Routes>
+    </>
   );
 }
 
