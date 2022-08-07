e6ebcee2223da480bf986761d79aea08e4655d88
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-08-07 17:47:38 +0530

    added: after login redirect

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,9 +1,11 @@
-import logo from "../assest/logo.png"
-import navbarImg from "../assest/navbar.png"
 import footerImg from "../assest/footer.png"
 import { useAuth } from "../context/AuthContext"
+import { NavbarSignIn } from "../components/NavbarSignIn"
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 export const SignInPage = ()=>{
-    const {googleSignIn} = useAuth();
+    const navigate=useNavigate();
+    const {googleSignIn,user} = useAuth();
 
     const googleSignHandler = async()=>{
         try{
@@ -13,14 +15,14 @@ export const SignInPage = ()=>{
             console.log(error)
         }
     }
+
+    useEffect(()=>{
+        if(user!== null)
+        navigate("/dashboard")
+    })
     return(<>
     <div>
-    <nav className="flex h-80">
-        <img className="w-20 h-20 lg:w-24 lg:h-24" src={logo} alt="logo" />
-        <div className="mx-auto">
-            <img className ="lg:w-screen lg:h-80" src={navbarImg} alt="nav-img" />
-        </div>
-    </nav>
+    <NavbarSignIn />
     <div className="md:w-52 md:ml-10 mx-4">
         <p className="text-xl mb-6">Welcome Back to Skrate!!</p>
         <button className="w-full bg-[#4F65F6] px-4 py-1 text-white rounded-sm" onClick={googleSignHandler}>Sign In With Google</button>
